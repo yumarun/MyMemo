52b7fe55c524a8fadc7aab2e22bb032ab0ae30ce
yuta <51759549+yumarun@users.noreply.github.com>
2022-07-19 08:51:43 +0900

    init 空vsアプリだけ追加

---
+++ MyMemo/MyMemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MyMemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            foreach(var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
